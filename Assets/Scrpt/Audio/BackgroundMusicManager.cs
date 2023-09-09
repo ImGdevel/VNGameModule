38b89afa905ec8500691a1f424bbc097a036b574
SH Woo <imdlsrks.mc@gmail.com>
2023-09-10 01:26:14 +0900

    Update BackgroundMusicManager.cs

--- Assets/Scrpt/Audio/BackgroundMusicManager.cs
+++ Assets/Scrpt/Audio/BackgroundMusicManager.cs
@@ -10,10 +10,10 @@ public class BackgroundMusicManager : MonoBehaviour
     private AudioSource audioSource;
     public MusicList musicList;
 
-    private string currentPlayingMusicName; // 현재 재생 중인 음악 이름
-    [Range(0f, 1f)] private float masterVolume = 1.0f;
-    [Range(0f, 1f)] private float musicVolume = 1.0f;
-    [Range(0f, 1f)] public float currentVolume;
+    private string currentPlayingMusicName;
+    private float masterVolume = 1.0f;
+    private float musicVolume = 1.0f;
+    private float currentVolume;
 
     const float fadeDuration = 2.0f;
 
@@ -46,7 +46,6 @@ public class BackgroundMusicManager : MonoBehaviour
 
     public void PlayMusic(string musicName) {
         if (musicName == currentPlayingMusicName) {
-            // 이미 해당 음악을 재생 중이면 아무 작업도 하지 않음
             return;
         }
 
@@ -58,29 +57,16 @@ public class BackgroundMusicManager : MonoBehaviour
             }
 
             if (currentPlayingMusicName != null) {
-                // 이전 음악을 서서히 페이드 아웃
-                fadeOutCoroutine = StartCoroutine(FadeOutMusic(() => {
-                    // 페이드 아웃이 완료되면 새로운 음악을 페이드 인하여 재생
-                    StartCoroutine(FadeInNewMusic(selectedMusic));
-                }));
+                CrossfadeToNewMusic(selectedMusic);
             }
             else {
-                // 이전 음악이 없는 경우, 즉시 음악 재생
-                audioSource.volume = 0.0f;
-                audioSource.clip = selectedMusic.audio;
-                audioSource.Play();
-
-                float finalSound = musicVolume * masterVolume;
-
-                StartCoroutine(FadeInMusic(finalSound)); // 볼륨 서서히 증가
+                StartNewMusic(selectedMusic);
             }
 
             currentPlayingMusicName = musicName;
-
             currentVolume = selectedMusic.volume;
         }
         else {
-            // 음악을 찾을 수 없는 경우
             Debug.LogWarning("음악을 찾을 수 없습니다: " + musicName);
         }
     }
@@ -107,14 +93,26 @@ public class BackgroundMusicManager : MonoBehaviour
         masterVolume = settings.soundSettings.masterVolume;
         musicVolume = settings.soundSettings.musicVolume;
 
-        // 설정이 변경될 때 현재 재생 중인 음악에 새로운 볼륨을 적용
         if (!string.IsNullOrEmpty(currentPlayingMusicName)) {
             float finalVolume = masterVolume * musicVolume * currentVolume;
             audioSource.volume = Mathf.Clamp01(finalVolume);
         }
     }
 
-    private IEnumerator FadeInMusic(float targetVolume ) {
+    private void CrossfadeToNewMusic(MusicList.Music newMusic) {
+        StartCoroutine(FadeOutMusic(() => {
+            StartCoroutine(FadeInNewMusic(newMusic));
+        }));
+    }
+
+    private void StartNewMusic(MusicList.Music newMusic) {
+        audioSource.volume = 0.0f;
+        audioSource.clip = newMusic.audio;
+        audioSource.Play();
+        StartCoroutine(FadeInMusic(musicVolume * masterVolume));
+    }
+
+    private IEnumerator FadeInMusic(float targetVolume) {
         float startVolume = audioSource.volume;
 
         float startTime = Time.time;
@@ -176,7 +174,6 @@ public class BackgroundMusicManager : MonoBehaviour
         audioSource.volume = 0.0f;
         audioSource.Stop();
 
-        // 페이드 아웃이 완료되면 콜백 함수 호출
         onFadeOutComplete?.Invoke();
     }
 }
