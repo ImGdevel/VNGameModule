1771461ad50dd1f8d83cb3f7efa4444cf1e570de
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-06-25 16:30:45 +0900

    temp save

--- Assets/Scrpt/Modual/BackgroundController.cs
+++ Assets/Scrpt/Modual/BackgroundController.cs
@@ -5,13 +5,52 @@ using VisualNovelGame;
 
 namespace VisualNovelGame
 {
-
     public class BackgroundController : MonoBehaviour, IController
     {
+        [SerializeField]
+        private SpriteList backgroundData;
+
+        private VNSpriteController spriteController;
+
+        private const float defaultDuration = 0.5f;
+
+        private void Awake()
+        {
+            GameObject spriteCotrollerObj = new GameObject("Event Scene");
+            VNSpriteController spriteController = spriteCotrollerObj.AddComponent<VNSpriteController>();
+            spriteController.SetSpriteList(backgroundData.spriteList);
+            spriteCotrollerObj.transform.SetParent(transform);
+        }
+
         public void SetScenario(Scenario scenario)
         {
             // 배경 관련 데이터 설정
+
+            // 배경 id와 배경 전환(나타나는) 이벤트에 따라서 변환
+
+
         }
+
+        public void SetBackground(int index)
+        {
+            spriteController.ShowSpriteInstant(index);
+        }
+
+        public void NextBackground(int index, float transitionDuration = defaultDuration)
+        {
+            StartCoroutine(spriteController.ChangeSpriteCrossFade(index, transitionDuration));
+        }
+
+        public void ChangeBackground(int index, float transitionDuration = defaultDuration)
+        {
+            StartCoroutine(spriteController.ChangeSpriteWithFadeToBlack(index, transitionDuration));
+        }
+
+        public void CloseBackground()
+        {
+            StartCoroutine(spriteController.FadeOutSprite());
+        }
+
     }
 
 
--- Assets/Scrpt/Modual/VisualNovelModule.cs
+++ Assets/Scrpt/Modual/VisualNovelModule.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 namespace VisualNovelGame
@@ -8,20 +10,283 @@ namespace VisualNovelGame
 
     public class VisualNovelModule : MonoBehaviour
     {
+        // Scenario Manager
         public ScenarioManager scenarioManager;
+
+        
+        private VNDialogManager dialogManager;
+
+        // Controller
         public ScenarioController scenarioController;
+        public DialogController dialogController;
+        public ChoiceController choiceController;
         public CharacterController characterController;
         public BackgroundController backgroundController;
-        public MusicController musicController;
+        public AudioController audioController;
+        
+
+        private List<DialogData> dialogueList;
+        private Dictionary<string, string> characterNames;
+        private Dictionary<string, List<EventData>> sceneEvents;
+        private int currentDialogueIndex = 0;
 
         private int currentScenarioId;
+        private string currentSceneName;
+
+        private bool isGamePaused = false;
+        private bool waitingForNextScene = false;
+        private bool autoScrollEnabled = false;
+        private bool onSceneSkipMove = false;
+        private bool isDialogueVisible = false;
+
+        private float typingSpeed = 0.03f;
+        private float autoScrollDelay = 2.0f;
+
+        private KeyCode nextDialogueKey = KeyCode.Space;
+        private KeyCode skipDialogueKey = KeyCode.LeftControl;
+        private KeyCode autoDialogueKey = KeyCode.A;
+        private KeyCode hideDialogueKey = KeyCode.Tab;
+
+        public static event Action OnFinishScenario;
+
+        void Awake()
+        {
+            if (dialogController == null) {
+                dialogController = FindObjectOfType<DialogController>();
+                Debug.LogError("DialogController not found.");
+            }
+            if (choiceController == null) {
+                choiceController = FindObjectOfType<ChoiceController>();
+                Debug.LogError("DialogController not found.");
+            }
+            if (backgroundController == null) {
+                backgroundController = FindObjectOfType<BackgroundController>();
+                Debug.LogError("BackgroundController not found.");
+            }
+            if (characterController == null) {
+                characterController = FindObjectOfType<CharacterController>();
+                Debug.LogError("CharacterController not found.");
+            }   
+            if (audioController == null) {
+                audioController = FindObjectOfType<AudioController>();
+                Debug.LogError("CharacterController not found.");
+            }
+        }
+
 
         void Start()
         {
-            LoadScenario(1); // 예시로 첫 번째 시나리오를 로드
+            //LoadScenario(1); // 예시로 첫 번째 시나리오를 로드
         }
 
-        public void LoadScenario(int scenarioId)
+        private void RegisterEventListeners()
+        {
+        }
+
+        private void OnDestroy()
+        {
+            UnregisterEventListeners();
+        }
+
+        private void UnregisterEventListeners()
+        {
+
+        }
+
+        private IEnumerator StartDialogueAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            StartDialogue();
+        }
+
+        private void StartDialogue()
+        {
+            ToggleDialogueUI();
+            ApplySettings(SettingsManager.GameSetting);
+            LoadDialogue(currentSceneName);
+            isGamePaused = false;
+        }
+
+        private void LoadDialogue(string loadName)
+        {
+            if (dialogManager == null) {
+                Debug.LogError("VNDialogManager is not assigned.");
+                return;
+            }
+            sceneEvents = dialogManager.GetSceneEvents();
+            dialogueList = dialogManager.GetSceneDialogs(loadName);
+
+            if (dialogueList != null && dialogueList.Count > 0) {
+                currentDialogueIndex = 0;
+                DialogData dialog = dialogueList[currentDialogueIndex];
+                if (Input.GetKey(skipDialogueKey)) {
+                    SkipScene(dialog);
+                }
+                else {
+                    PlayScene(dialog);
+                }
+            }
+            else {
+                Debug.LogWarning("No dialogue data found for the current scene.");
+            }
+        }
+
+        void Update()
+        {
+            if (isGamePaused) {
+                return;
+            }
+
+            if (Input.GetKeyDown(nextDialogueKey)) {
+                PlayScene(dialogueList[currentDialogueIndex]);
+            }
+
+            if (Input.GetKey(skipDialogueKey)) {
+                onSceneSkipMove = true;
+                SkipScene(dialogueList[currentDialogueIndex]);
+            }
+
+            if (Input.GetKeyUp(skipDialogueKey)) {
+                onSceneSkipMove = false;
+            }
+
+            if (!autoScrollEnabled && Input.GetKeyDown(autoDialogueKey)) {
+                autoScrollEnabled = true;
+                StopAllCoroutines();
+                StartCoroutine(AutoPlayScene());
+            }
+            else if (autoScrollEnabled && Input.anyKeyDown) {
+                autoScrollEnabled = false;
+                StopCoroutine("AutoPlayScene");
+            }
+
+            if (Input.GetKeyDown(hideDialogueKey)) {
+                ToggleDialogueUI();
+            }
+        }
+
+        public void NextDialogue()
+        {
+            currentDialogueIndex = (currentDialogueIndex + 1) % dialogueList.Count;
+            waitingForNextScene = false;
+            OnFinishScenario?.Invoke();
+        }
+
+        private void PlayScene(DialogData dialog)
+        {
+            Debug.Log("Current dialogue number: " + currentSceneName + "(" + dialog.id + ")");
+            if (dialog.choices.Count == 0) {
+                if (sceneEvents.ContainsKey(dialog.id)) {
+                    PlaySceneEvent(sceneEvents[dialog.id]);
+                }
+
+                dialogController.TypeDialogue(dialog.character, dialog.content, typingSpeed);
+            }
+            else {
+                ChoiceScene(dialog);
+            }
+        }
+
+        private void SkipScene(DialogData dialog)
+        {
+            if (dialog.choices.Count == 0) {
+                if (Input.GetKeyDown(KeyCode.LeftControl)) {
+                    dialogController.CurrentDialogueSkip();
+                }
+                else {
+                    if (sceneEvents.ContainsKey(dialog.id)) {
+                        PlaySceneEvent(sceneEvents[dialog.id]);
+                    }
+                    dialogController.SkipDialogue(dialog.character, dialog.content);
+                }
+            }
+            else {
+                ChoiceScene(dialog);
+            }
+        }
+
+        private void PlaySceneEvent(List<EventData> eventDatas)
+        {
+
+        }
+
+        private void SceneChange(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        private void ChoiceScene(DialogData dialog)
+        {
+            isGamePaused = true;
+            choiceController.transform.gameObject.SetActive(true);
+            choiceController.ShowChoices(dialog.choices);
+            dialogController.ClearDialogue();
+        }
+
+        public void JumpScene(string jump_id)
+        {
+            choiceController.transform.gameObject.SetActive(false);
+            isGamePaused = false;
+
+            for (int i = 0; i < dialogueList.Count; i++) {
+                if (dialogueList[i].id == jump_id) {
+                    currentDialogueIndex = i;
+                    dialogController.ClearDialogue();
+                    PlayScene(dialogueList[currentDialogueIndex]);
+                    break;
+                }
+            }
+        }
+
+        private IEnumerator AutoPlayScene()
+        {
+            while (autoScrollEnabled) {
+                PlayScene(dialogueList[currentDialogueIndex]);
+
+                waitingForNextScene = true;
+                yield return new WaitUntil(() => !waitingForNextScene);
+                yield return new WaitForSeconds(autoScrollDelay);
+            }
+        }
+
+        public void ToggleGamePause(bool state)
+        {
+            isGamePaused = state;
+        }
+
+        public void ApplySettings(Settings settings)
+        {
+            typingSpeed = settings.dialogueSettings.typingSpeed;
+            autoScrollDelay = settings.dialogueSettings.dialogueDelay;
+
+            autoDialogueKey = settings.controlSettings.AutoDialogKeyCode;
+            nextDialogueKey = settings.controlSettings.NextDialogKeyCode;
+            skipDialogueKey = settings.controlSettings.SkipKeyCode;
+            hideDialogueKey = settings.controlSettings.HideUIKeyCode;
+        }
+
+        private void ToggleDialogueUI()
+        {
+            isDialogueVisible = !isDialogueVisible;
+            // 대화창 숨기거나 보이기
+        }
+
+        public void SaveDialogueData(GameData saveData)
+        {
+            Debug.Log("대화 데이터 저장");
+
+        }
+
+        public void LoadDialogueData(GameData saveData)
+        {
+            Debug.Log("대화 데이터 불러오기 적용");
+
+        }
+
+    }
+
+    /*
+    public void LoadScenario(int scenarioId)
         {
             Scenario scenario = scenarioManager.GetScenario(scenarioId);
             if (scenario != null) {
@@ -29,7 +294,7 @@ namespace VisualNovelGame
                 scenarioController.SetScenario(scenario);
                 characterController.SetScenario(scenario);
                 backgroundController.SetScenario(scenario);
-                musicController.SetScenario(scenario);
+                audioController.SetScenario(scenario);
             }
         }
 
@@ -41,7 +306,7 @@ namespace VisualNovelGame
             }
         }
     }
-
+    */
 
 }
 
