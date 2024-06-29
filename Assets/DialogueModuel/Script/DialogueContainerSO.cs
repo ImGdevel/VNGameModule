dd3be95afc72b8157dc2a835695b61a06cd91157
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-06-29 22:54:51 +0900

    comment

--- Assets/DialogueModuel/Script/DialogueContainerSO.cs
+++ Assets/DialogueModuel/Script/DialogueContainerSO.cs
@@ -15,27 +15,27 @@ using MeetAndTalk.Event;
 
 namespace MeetAndTalk
 {
+    // 대화 컨테이너를 생성할 수 있는 ScriptableObject
     [CreateAssetMenu(menuName = "Dialogue/New Dialogue")]
     [System.Serializable]
     public class DialogueContainerSO : ScriptableObject
     {
-        public List<NodeLinkData> NodeLinkDatas = new List<NodeLinkData>();
+        public List<NodeLinkData> NodeLinkDatas = new List<NodeLinkData>(); // 노드 간 연결 데이터를 저장하는 리스트
+        public List<DialogueChoiceNodeData> DialogueChoiceNodeDatas = new List<DialogueChoiceNodeData>(); // 대화 선택 노드 데이터를 저장하는 리스트
+        public List<DialogueNodeData> DialogueNodeDatas = new List<DialogueNodeData>(); // 대화 노드 데이터를 저장하는 리스트
+        public List<EndNodeData> EndNodeDatas = new List<EndNodeData>(); // 종료 노드 데이터를 저장하는 리스트
+        public List<StartNodeData> StartNodeDatas = new List<StartNodeData>(); // 시작 노드 데이터를 저장하는 리스트
 
-        public List<DialogueChoiceNodeData> DialogueChoiceNodeDatas = new List<DialogueChoiceNodeData>();
-        public List<DialogueNodeData> DialogueNodeDatas = new List<DialogueNodeData>();
-        public List<EndNodeData> EndNodeDatas = new List<EndNodeData>();
-        public List<StartNodeData> StartNodeDatas = new List<StartNodeData>();
         [Header("Pro Version Node")]
-        public List<TimerChoiceNodeData> TimerChoiceNodeDatas = new List<TimerChoiceNodeData>();
-        public List<EventNodeData> EventNodeDatas = new List<EventNodeData>();
-        public List<RandomNodeData> RandomNodeDatas = new List<RandomNodeData>();
-        public List<CommandNodeData> CommandNodeDatas = new List<CommandNodeData>();
-        public List<IfNodeData> IFNodeDatas = new List<IfNodeData>();
+        public List<TimerChoiceNodeData> TimerChoiceNodeDatas = new List<TimerChoiceNodeData>(); // 타이머 선택 노드 데이터를 저장하는 리스트
+        public List<EventNodeData> EventNodeDatas = new List<EventNodeData>(); // 이벤트 노드 데이터를 저장하는 리스트
+        public List<RandomNodeData> RandomNodeDatas = new List<RandomNodeData>(); // 랜덤 노드 데이터를 저장하는 리스트
+        public List<CommandNodeData> CommandNodeDatas = new List<CommandNodeData>(); // 명령 노드 데이터를 저장하는 리스트
+        public List<IfNodeData> IFNodeDatas = new List<IfNodeData>(); // 조건 노드 데이터를 저장하는 리스트
 
-        public List<BaseNodeData> AllNodes
-        {
-            get
-            {
+        // 모든 노드 데이터를 반환하는 프로퍼티
+        public List<BaseNodeData> AllNodes {
+            get {
                 List<BaseNodeData> tmp = new List<BaseNodeData>();
                 tmp.AddRange(DialogueNodeDatas);
                 tmp.AddRange(DialogueChoiceNodeDatas);
@@ -50,128 +50,146 @@ namespace MeetAndTalk
             }
         }
     }
+
+    // 노드 간의 연결 정보를 저장하는 클래스
     [System.Serializable]
     public class NodeLinkData
     {
-        public string BaseNodeGuid;
-        public string TargetNodeGuid;
+        public string BaseNodeGuid; // 기준 노드의 GUID
+        public string TargetNodeGuid; // 대상 노드의 GUID
     }
 
+    // 모든 노드의 기본 클래스
     [System.Serializable]
     public class BaseNodeData
     {
-        public string NodeGuid;
-        public Vector2 Position;
+        public string NodeGuid; // 노드의 GUID
+        public Vector2 Position; // 노드의 위치
     }
 
+    // 대화 선택 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class DialogueChoiceNodeData : BaseNodeData
     {
-        public List<DialogueNodePort> DialogueNodePorts;
-        public List<LanguageGeneric<AudioClip>> AudioClips;
-        public DialogueCharacterSO Character;
-        public AvatarPosition AvatarPos;
-        public AvatarType AvatarType;
-        public List<LanguageGeneric<string>> TextType;
-        public float Duration;
+        public List<DialogueNodePort> DialogueNodePorts; // 대화 노드 포트 리스트
+        public List<LanguageGeneric<AudioClip>> AudioClips; // 오디오 클립 리스트
+        public DialogueCharacterSO Character; // 대화 캐릭터 정보
+        public AvatarPosition AvatarPos; // 아바타 위치
+        public AvatarType AvatarType; // 아바타 타입
+        public List<LanguageGeneric<string>> TextType; // 대화 텍스트 리스트
+        public float Duration; // 노드 지속 시간
     }
 
+    // 타이머 선택 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class TimerChoiceNodeData : BaseNodeData
     {
-        public List<DialogueNodePort> DialogueNodePorts;
-        public List<LanguageGeneric<AudioClip>> AudioClips;
-        public DialogueCharacterSO Character;
-        public AvatarPosition AvatarPos;
-        public AvatarType AvatarType;
-        public List<LanguageGeneric<string>> TextType;
-        public float Duration;
-        public float time;
+        public List<DialogueNodePort> DialogueNodePorts; // 대화 노드 포트 리스트
+        public List<LanguageGeneric<AudioClip>> AudioClips; // 오디오 클립 리스트
+        public DialogueCharacterSO Character; // 대화 캐릭터 정보
+        public AvatarPosition AvatarPos; // 아바타 위치
+        public AvatarType AvatarType; // 아바타 타입
+        public List<LanguageGeneric<string>> TextType; // 대화 텍스트 리스트
+        public float Duration; // 노드 지속 시간
+        public float time; // 타이머 시간
     }
 
+    // 랜덤 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class RandomNodeData : BaseNodeData
     {
-        public List<DialogueNodePort> DialogueNodePorts;
+        public List<DialogueNodePort> DialogueNodePorts; // 대화 노드 포트 리스트
     }
 
+    // 대화 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class DialogueNodeData : BaseNodeData
     {
-        public List<DialogueNodePort> DialogueNodePorts;
-        public List<LanguageGeneric<AudioClip>> AudioClips;
-        public DialogueCharacterSO Character;
-        public AvatarPosition AvatarPos;
-        public AvatarType AvatarType;
-        public List<LanguageGeneric<string>> TextType;
-        public float Duration;
+        public List<DialogueNodePort> DialogueNodePorts; // 대화 노드 포트 리스트
+        public List<LanguageGeneric<AudioClip>> AudioClips; // 오디오 클립 리스트
+        public DialogueCharacterSO Character; // 대화 캐릭터 정보
+        public AvatarPosition AvatarPos; // 아바타 위치
+        public AvatarType AvatarType; // 아바타 타입
+        public List<LanguageGeneric<string>> TextType; // 대화 텍스트 리스트
+        public float Duration; // 노드 지속 시간
     }
 
+    // 종료 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class EndNodeData : BaseNodeData
     {
-        public EndNodeType EndNodeType;
+        public EndNodeType EndNodeType; // 종료 노드 타입 (End, Repeat, GoBack, ReturnToStart)
     }
 
+    // 시작 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class StartNodeData : BaseNodeData
     {
-        public string startID;
+        public string startID; // 시작 노드 ID
     }
 
-
+    // 이벤트 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class EventNodeData : BaseNodeData
     {
-        public List<EventScriptableObjectData> EventScriptableObjects;
+        public List<EventScriptableObjectData> EventScriptableObjects; // 이벤트 ScriptableObject 리스트
     }
+
+    // 이벤트 ScriptableObject 데이터를 저장하는 클래스
     [System.Serializable]
     public class EventScriptableObjectData
     {
-        public DialogueEventSO DialogueEventSO;
+        public DialogueEventSO DialogueEventSO; // 대화 이벤트 ScriptableObject
     }
 
+    // 명령 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class CommandNodeData : BaseNodeData
     {
-        public string commmand;
+        public string commmand; // 명령 문자열
     }
+
+    // 조건 노드 데이터를 저장하는 클래스
     [System.Serializable]
     public class IfNodeData : BaseNodeData
     {
-        public string ValueName;
-        public GlobalValueIFOperations Operations;
-        public string OperationValue;
+        public string ValueName; // 조건 값 이름
+        public GlobalValueIFOperations Operations; // 조건 연산자
+        public string OperationValue; // 조건 값
 
-        public string TrueGUID;
-        public string FalseGUID;
+        public string TrueGUID; // 조건이 참일 때 연결되는 노드 GUID
+        public string FalseGUID; // 조건이 거짓일 때 연결되는 노드 GUID
     }
 
+    // 언어별 제네릭 타입을 저장하는 클래스
     [System.Serializable]
     public class LanguageGeneric<T>
     {
-        public LocalizationEnum languageEnum;
-        public T LanguageGenericType;
+        public LocalizationEnum languageEnum; // 언어 Enum
+        public T LanguageGenericType; // 언어별 제네릭 타입
     }
 
+    // 대화 노드 포트를 저장하는 클래스
     [System.Serializable]
     public class DialogueNodePort
     {
-        public string InputGuid;
-        public string OutputGuid;
+        public string InputGuid; // 입력 GUID
+        public string OutputGuid; // 출력 GUID
+
 #if UNITY_EDITOR
-        [HideInInspector] public Port MyPort;
+        [HideInInspector] public Port MyPort; // 에디터 전용 포트
 #endif
-        public TextField TextField;
-        public List<LanguageGeneric<string>> TextLanguage = new List<LanguageGeneric<string>>();
+        public TextField TextField; // 텍스트 필드
+        public List<LanguageGeneric<string>> TextLanguage = new List<LanguageGeneric<string>>(); // 언어별 텍스트 리스트
     }
 
+    // 종료 노드 타입 Enum
     [System.Serializable]
     public enum EndNodeType
     {
-        End,
-        Repeat,
-        GoBack,
-        ReturnToStart
+        End, // 종료
+        Repeat, // 반복
+        GoBack, // 돌아가기
+        ReturnToStart // 시작으로 돌아가기
     }
 }
--- Assets/DialogueModuel/Script/DialogueManager.cs
+++ Assets/DialogueModuel/Script/DialogueManager.cs
@@ -10,74 +10,78 @@ namespace MeetAndTalk
 {
     public class DialogueManager : DialogueGetData
     {
-        [HideInInspector] public static DialogueManager Instance;
-        public LocalizationManager localizationManager;
+        [HideInInspector] public static DialogueManager Instance; // 싱글톤 인스턴스
+        public LocalizationManager localizationManager; // LocalizationManager 인스턴스
 
-        [HideInInspector] public DialogueUIManager dialogueUIManager;
-        public AudioSource audioSource;
+        [HideInInspector] public DialogueUIManager dialogueUIManager; // DialogueUIManager 인스턴스
+        public AudioSource audioSource; // 오디오 소스 컴포넌트
 
-        public UnityEvent StartDialogueEvent;
-        public UnityEvent EndDialogueEvent;
+        public UnityEvent StartDialogueEvent; // 대화 시작 이벤트
+        public UnityEvent EndDialogueEvent; // 대화 종료 이벤트
 
-        private BaseNodeData currentDialogueNodeData;
-        private BaseNodeData lastDialogueNodeData;
+        private BaseNodeData currentDialogueNodeData; // 현재 대화 노드 데이터
+        private BaseNodeData lastDialogueNodeData; // 마지막 대화 노드 데이터
 
-        private TimerChoiceNodeData _nodeTimerInvoke;
-        private DialogueNodeData _nodeDialogueInvoke;
-        private DialogueChoiceNodeData _nodeChoiceInvoke;
+        private TimerChoiceNodeData _nodeTimerInvoke; // 타이머 선택 노드 데이터
+        private DialogueNodeData _nodeDialogueInvoke; // 대화 노드 데이터
+        private DialogueChoiceNodeData _nodeChoiceInvoke; // 대화 선택 노드 데이터
 
-        float Timer;
+        float Timer; // 타이머 변수
 
         private void Awake()
         {
-            Instance = this;
-            dialogueUIManager= DialogueUIManager.Instance;
-            audioSource = GetComponent<AudioSource>();
+            Instance = this; // 싱글톤 인스턴스 설정
+            dialogueUIManager = DialogueUIManager.Instance; // DialogueUIManager 인스턴스 설정
+            audioSource = GetComponent<AudioSource>(); // 오디오 소스 컴포넌트 가져오기
         }
 
         private void Update()
         {
-            Timer -= Time.deltaTime;
-            if (Timer > 0) dialogueUIManager.TimerSlider.value = Timer;
+            Timer -= Time.deltaTime; // 타이머 감소
+            if (Timer > 0) dialogueUIManager.TimerSlider.value = Timer; // 타이머 슬라이더 업데이트
         }
 
         public void SetupDialogue(DialogueContainerSO dialogue)
         {
-            dialogueContainer = dialogue;
+            dialogueContainer = dialogue; // 대화 컨테이너 설정
         }
 
         public void StartDialogue(DialogueContainerSO dialogue)
         {
-            dialogueUIManager = DialogueUIManager.Instance;
-            dialogueContainer = dialogue;
+            dialogueUIManager = DialogueUIManager.Instance; // DialogueUIManager 인스턴스 설정
+            dialogueContainer = dialogue; // 대화 컨테이너 설정
 
+            // 시작 노드 데이터가 하나일 경우
             if (dialogueContainer.StartNodeDatas.Count == 1) CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[0]));
+            // 시작 노드 데이터가 여러 개일 경우 무작위로 선택
             else { CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[Random.Range(0, dialogueContainer.StartNodeDatas.Count)])); }
 
-            dialogueUIManager.dialogueCanvas .SetActive(true);
-            StartDialogueEvent.Invoke();
+            dialogueUIManager.dialogueCanvas.SetActive(true); // 대화 캔버스 활성화
+            StartDialogueEvent.Invoke(); // 대화 시작 이벤트 호출
         }
 
         public void StartDialogue(string ID)
         {
-            StartDialogue();
+            StartDialogue(); // ID로 대화 시작
         }
 
         public void StartDialogue()
         {
-            dialogueUIManager= DialogueUIManager.Instance;
+            dialogueUIManager = DialogueUIManager.Instance; // DialogueUIManager 인스턴스 설정
 
+            // 시작 노드 데이터가 하나일 경우
             if (dialogueContainer.StartNodeDatas.Count == 1) CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[0]));
+            // 시작 노드 데이터가 여러 개일 경우 무작위로 선택
             else { CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[Random.Range(0, dialogueContainer.StartNodeDatas.Count)])); }
 
-            dialogueUIManager.dialogueCanvas.SetActive(true);
-            StartDialogueEvent.Invoke();
+            dialogueUIManager.dialogueCanvas.SetActive(true); // 대화 캔버스 활성화
+            StartDialogueEvent.Invoke(); // 대화 시작 이벤트 호출
         }
 
         public void CheckNodeType(BaseNodeData _baseNodeData)
         {
-            switch (_baseNodeData)
-            {
+            // 노드 타입에 따라 실행할 함수 선택
+            switch (_baseNodeData) {
                 case StartNodeData nodeData:
                     RunNode(nodeData);
                     break;
@@ -95,119 +99,138 @@ namespace MeetAndTalk
             }
         }
 
-
         private void RunNode(StartNodeData _nodeData)
         {
-            CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[0]));
+            CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[0])); // 다음 노드 실행
         }
 
         private void RunNode(DialogueNodeData _nodeData)
         {
-            lastDialogueNodeData = currentDialogueNodeData;
-            currentDialogueNodeData = _nodeData;
+            lastDialogueNodeData = currentDialogueNodeData; // 마지막 노드 데이터 업데이트
+            currentDialogueNodeData = _nodeData; // 현재 노드 데이터 업데이트
 
-            // Normal Multiline
-            if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) { dialogueUIManager.ResetText(""); dialogueUIManager.nameTextBox.text = $"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}</color>"; }
-            // No Change Character Multiline
-            else if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) { dialogueUIManager.ResetText(""); }
-            // Normal Inline
-            else if (_nodeData.Character != null) dialogueUIManager.ResetText($"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}: </color>");
-            // Last Change
-            else dialogueUIManager.ResetText("");
+            // 캐릭터 이름과 대화 텍스트 설정
+            if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) {
+                dialogueUIManager.ResetText("");
+                dialogueUIManager.nameTextBox.text = $"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}</color>";
+            }
+            else if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) {
+                dialogueUIManager.ResetText("");
+            }
+            else if (_nodeData.Character != null) {
+                dialogueUIManager.ResetText($"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}: </color>");
+            }
+            else {
+                dialogueUIManager.ResetText("");
+            }
 
+            // 전체 대화 텍스트 설정
             dialogueUIManager.fullText = $"{_nodeData.TextType.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}";
 
-            dialogueUIManager.SkipButton.SetActive(true);
-            MakeButtons(new List<DialogueNodePort>());
+            dialogueUIManager.SkipButton.SetActive(true); // 스킵 버튼 활성화
+            MakeButtons(new List<DialogueNodePort>()); // 버튼 생성
 
-            if(_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType != null) audioSource.PlayOneShot(_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType);
+            // 오디오 클립 재생
+            if (_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType != null)
+                audioSource.PlayOneShot(_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType);
 
-            _nodeDialogueInvoke = _nodeData;
+            _nodeDialogueInvoke = _nodeData; // 대화 노드 데이터 설정
 
+            // 노드 지속 시간 후 다음 노드로 이동
             IEnumerator tmp() { yield return new WaitForSeconds(_nodeData.Duration); DialogueNode_NextNode(); }
-            if(_nodeData.Duration != 0) StartCoroutine(tmp());
+            if (_nodeData.Duration != 0) StartCoroutine(tmp());
         }
+
         private void RunNode(DialogueChoiceNodeData _nodeData)
         {
-            lastDialogueNodeData = currentDialogueNodeData;
-            currentDialogueNodeData = _nodeData;
+            lastDialogueNodeData = currentDialogueNodeData; // 마지막 노드 데이터 업데이트
+            currentDialogueNodeData = _nodeData; // 현재 노드 데이터 업데이트
 
-            // Normal Multiline
-            if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) { dialogueUIManager.ResetText(""); dialogueUIManager.nameTextBox.text = $"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}</color>"; }
-            // No Change Character Multiline
-            else if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) { dialogueUIManager.ResetText(""); }
-            // Normal Inline
-            else if (_nodeData.Character != null) dialogueUIManager.ResetText($"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}: </color>");
-            // Last Change
-            else dialogueUIManager.ResetText("");
+            // 캐릭터 이름과 대화 텍스트 설정
+            if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) {
+                dialogueUIManager.ResetText("");
+                dialogueUIManager.nameTextBox.text = $"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}</color>";
+            }
+            else if (dialogueUIManager.showSeparateName && dialogueUIManager.nameTextBox != null && _nodeData.Character != null) {
+                dialogueUIManager.ResetText("");
+            }
+            else if (_nodeData.Character != null) {
+                dialogueUIManager.ResetText($"<color={_nodeData.Character.HexColor()}>{_nodeData.Character.characterName.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}: </color>");
+            }
+            else {
+                dialogueUIManager.ResetText("");
+            }
 
+            // 전체 대화 텍스트 설정
             dialogueUIManager.fullText = $"{_nodeData.TextType.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType}";
 
-            dialogueUIManager.SkipButton.SetActive(true);
-            MakeButtons(new List<DialogueNodePort>());
+            dialogueUIManager.SkipButton.SetActive(true); // 스킵 버튼 활성화
+            MakeButtons(new List<DialogueNodePort>()); // 버튼 생성
 
-            _nodeChoiceInvoke = _nodeData;
+            _nodeChoiceInvoke = _nodeData; // 대화 선택 노드 데이터 설정
 
+            // 노드 지속 시간 후 선택지 생성
             IEnumerator tmp() { yield return new WaitForSeconds(_nodeData.Duration); ChoiceNode_GenerateChoice(); }
             StartCoroutine(tmp());
 
-            if (_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType != null) audioSource.PlayOneShot(_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType);
+            // 오디오 클립 재생
+            if (_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType != null)
+                audioSource.PlayOneShot(_nodeData.AudioClips.Find(clip => clip.languageEnum == localizationManager.SelectedLang()).LanguageGenericType);
         }
 
         private void RunNode(EndNodeData _nodeData)
         {
-            switch (_nodeData.EndNodeType)
-            {
+            // 엔드 노드 타입에 따라 다른 동작 수행
+            switch (_nodeData.EndNodeType) {
                 case EndNodeType.End:
-                    dialogueUIManager.dialogueCanvas.SetActive(false);
-                    EndDialogueEvent.Invoke();
+                    dialogueUIManager.dialogueCanvas.SetActive(false); // 대화 캔버스 비활성화
+                    EndDialogueEvent.Invoke(); // 대화 종료 이벤트 호출
                     break;
                 case EndNodeType.Repeat:
-                    CheckNodeType(GetNodeByGuid(currentDialogueNodeData.NodeGuid));
+                    CheckNodeType(GetNodeByGuid(currentDialogueNodeData.NodeGuid)); // 현재 노드 반복 실행
                     break;
                 case EndNodeType.GoBack:
-                    CheckNodeType(GetNodeByGuid(lastDialogueNodeData.NodeGuid));
+                    CheckNodeType(GetNodeByGuid(lastDialogueNodeData.NodeGuid)); // 마지막 노드로 돌아가기
                     break;
                 case EndNodeType.ReturnToStart:
-                    CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[Random.Range(0,dialogueContainer.StartNodeDatas.Count)]));
+                    CheckNodeType(GetNextNode(dialogueContainer.StartNodeDatas[Random.Range(0, dialogueContainer.StartNodeDatas.Count)])); // 시작 노드로 돌아가기
                     break;
                 default:
                     break;
             }
         }
 
-
         private void MakeButtons(List<DialogueNodePort> _nodePorts)
         {
-            List<string> texts = new List<string>();
-            List<UnityAction> unityActions = new List<UnityAction>();
+            List<string> texts = new List<string>(); // 버튼 텍스트 리스트
+            List<UnityAction> unityActions = new List<UnityAction>(); // 버튼 액션 리스트
 
-            foreach (DialogueNodePort nodePort in _nodePorts)
-            {
+            // 각 노드 포트에 대해 버튼 텍스트와 액션 설정
+            foreach (DialogueNodePort nodePort in _nodePorts) {
                 texts.Add(nodePort.TextLanguage.Find(text => text.languageEnum == localizationManager.SelectedLang()).LanguageGenericType);
                 UnityAction tempAction = null;
-                tempAction += () =>
-                {
-                    CheckNodeType(GetNodeByGuid(nodePort.InputGuid));
+                tempAction += () => {
+                    CheckNodeType(GetNodeByGuid(nodePort.InputGuid)); // 노드 포트의 GUID를 사용하여 다음 노드로 이동
                 };
                 unityActions.Add(tempAction);
             }
 
-            dialogueUIManager.SetButtons(texts, unityActions, false);
+            dialogueUIManager.SetButtons(texts, unityActions, false); // 버튼 설정
         }
 
-
-        void DialogueNode_NextNode() { CheckNodeType(GetNextNode(_nodeDialogueInvoke)); }
-        void ChoiceNode_GenerateChoice() { MakeButtons(_nodeChoiceInvoke.DialogueNodePorts);
-            dialogueUIManager.SkipButton.SetActive(false);
+        void DialogueNode_NextNode() { CheckNodeType(GetNextNode(_nodeDialogueInvoke)); } // 다음 대화 노드 실행
+        void ChoiceNode_GenerateChoice()
+        {
+            MakeButtons(_nodeChoiceInvoke.DialogueNodePorts); // 선택지 생성
+            dialogueUIManager.SkipButton.SetActive(false); // 스킵 버튼 비활성화
         }
 
         public void SkipDialogue()
         {
-            StopAllCoroutines();
+            StopAllCoroutines(); // 모든 코루틴 중지
 
-            switch (currentDialogueNodeData)
-            {
+            // 현재 대화 노드 데이터에 따라 다음 동작 수행
+            switch (currentDialogueNodeData) {
                 case DialogueNodeData nodeData:
                     DialogueNode_NextNode();
                     break;
@@ -218,10 +241,11 @@ namespace MeetAndTalk
                     break;
             }
         }
+
         public void ForceEndDialog()
         {
-            dialogueUIManager.dialogueCanvas.SetActive(false);
-            EndDialogueEvent.Invoke();
+            dialogueUIManager.dialogueCanvas.SetActive(false); // 대화 캔버스 비활성화
+            EndDialogueEvent.Invoke(); // 대화 종료 이벤트 호출
         }
     }
 }
